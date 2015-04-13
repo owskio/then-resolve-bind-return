d45fc7105b77cd97d506d6b40e8041ef03ee359d
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-04-13 07:57:55 -0500

    Splitting out dependencies from promise lib

---
+++ expose.js
@@ -0,0 +1,26 @@
+
+var
+
+argList = require('./argList'),
+
+mport = function(obj,fn){
+  if (typeof(fn) === 'undefined') {
+    return function(fn2){
+      return mport(obj,fn2);
+    };
+  } else {
+    var results = [],
+    desired = argList(fn);
+    for(var i in desired){
+      results.push(obj[desired[i]]);
+    }
+    return fn.apply(this,results);
+  }
+},
+expose = function(mod,obj){
+  obj.mport = mport(obj);
+  mod.exports = obj;
+  return obj;
+};
+
+module.exports = expose;
