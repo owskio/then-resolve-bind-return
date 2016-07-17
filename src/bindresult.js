6bba97edaf650a11ffb4bcc869675e7a6b05ecbd
ted <theodore.cackowski@gmail.com>
2016-07-17 05:10:10 -0500

    removed log statements, added clearer var names

--- src/bindresult.js
+++ src/bindresult.js
@@ -1,29 +1,20 @@
 
 
 //assume a .thenable as the 'monadic' value
-
 var bind = function(monadicFn,callback){
-    console.log('monadicFn: ',monadicFn);
-    console.log('callback: ',callback);
     return function(input){
-        console.log('input: ',input);
         var self = this;
         var thenable = monadicFn(input);
         return thenable.then(function(monadicValue){
-            console.log('monadicValue: ',monadicValue);
-            return callback.apply(self,[monadicValue])(input);
+            var nextMonadicFn = callback.apply(self,[monadicValue])
+            return nextMonadicFn(input);
         });
     }; 
 };
 
-//var result = function(actualResultValue){
-//    console.log('4');
-//    return Promise.resolve(actualResultValue);
-//};
+//This is basically just the K(M a) combinator
 var result = function(actualResultValue){
-    console.log('4');
     return function(dontCare){
-    console.log('5');
         return Promise.resolve(actualResultValue);
     };
 };
