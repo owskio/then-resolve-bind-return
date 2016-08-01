ee6119fd73fcb2d0d2a5e19c890d475f3cea58d6
ted <theodore.cackowski@gmail.com>
2016-08-01 03:07:02 -0500

    unit test cleanup

--- tests/expose.mocha.js
+++ tests/expose.mocha.js
@@ -10,18 +10,16 @@ var p2 = Promise.resolve('again!');
 
 describe('bindResult',function(){
     
-    it('should bind existing promises',function(done){
-        //Just a simple 'nominal test case'
-        var myModule = 
+    it('should nominally bind existing promises',function(done){
+        var nominalCase = 
                bind(p1,function(trying){
         return bind(p2,function(again){
         return result(trying + again);
         });});
         
-        bind(myModule,function(unwrapped){
+        bind(nominalCase,function(unwrapped){
             unwrapped.must.eql('trying again!');
-            done();
-            return result('Meh, must have passed');
+            return result(done());
         });
         
     });
@@ -29,28 +27,19 @@ describe('bindResult',function(){
   it('should obey right identity',function(done){
     //https://wiki.haskell.org/Monad_laws
      
-    var shouldSayTrying = 
-           bind(p1,function(trying){
-      return result(trying);
-    });
+    //And this would be the "m" mentioned in the link above
+    var tyingPromise = p1;
       
     //This one is the "m >>= return" mentioned in the link above
-    var shouldSayTryingAgain = bind(p1,result);
-      
-    //And this would be the "m" mentioned in the link above
-    var shouldAlsoSayTrying = p1;
+    var tryingRedundant = bind(p1,result);
       
     //Bind everything together just to make the assertion
-    var test =
-           bind(shouldSayTrying,function(tryingLambdaResult){
-    return bind(shouldSayTryingAgain,function(tryingResult){
-    return bind(shouldAlsoSayTrying,function(trying){
+    var assertion =
+           bind(tyingPromise,function(trying){
+    return bind(tryingRedundant,function(tryingResult){
         trying.must.eql(tryingResult);
-        trying.must.eql(tryingLambdaResult);
-        tryingResult.must.eql(tryingLambdaResult);
-        done();
-        return result('Meh, no exceptions, must have passed');
-    });});});
+        return result(done());
+    });});
   });
 
   it('should obey left identity',function(done){
@@ -70,49 +59,33 @@ describe('bindResult',function(){
     var assert = 
            bind(helloWorldMaybe,function(helloWorldMaybe){
     return bind(helloWorldDefinitely,function(helloWorldDefinitely){
-      
         helloWorldDefinitely.must.eql(helloWorldMaybe);
-        done();
-        return result('Meh, must have passed');
+        return result(done());
     });});
   });
 
-    
   it('should obey associativity',function(done){
     //https://wiki.haskell.org/Monad_laws
-      
-    var addHello = Promise.resolve('Hello'); 
-      
+    var hello = result('Hello'); 
     var addBeautiful = function(hello){
-      console.log('returning hello beautiful: ',hello);
-      return Promise.resolve(hello + ' beautiful ');
+      return result(hello + ' beautiful ');
     };
     var addWorld = function(helloBeautiful){
-      console.log('returning hello beautiful world: ',helloBeautiful);
-      return Promise.resolve(helloBeautiful + ' world!');
+      return result(helloBeautiful + ' world!');
     };
 
-    var firstTwo = bind(addHello,addBeautiful);
-      console.log('firstTwo: ',firstTwo);
-    var leftAssociative = 
-        bind(
-            firstTwo,
-            addWorld
-        );
+    var leftAssociative = bind(bind(hello,addBeautiful),addWorld);
     var rightAssociative = 
-        bind(addHello,function(hello){
-            return bind(addBeautiful(hello),addWorld);
-        });
+           bind(hello,function(hello){
+    return bind(addBeautiful(hello),addWorld);
+    });
       
     //Bind everything together for unit testing purposes
     var assert = 
            bind(leftAssociative,function(firstGreeting){
-               console.log('firstGreeting: ',firstGreeting);
     return bind(rightAssociative,function(secondGreeting){
-      
         firstGreeting.must.eql(secondGreeting);
-        done();
-        return result('Meh, must have passed');
+        return result(done());
     });});
 
   });
